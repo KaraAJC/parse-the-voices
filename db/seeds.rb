99082a64e15f4ba99c78e64ce5094c4d2ba3445c
Loraine <loraine@lorainekv.com>
2015-10-03 12:35:44 -0700

    Have a way to seed our data into a postgres db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require "spreadsheet"
+
+book = Spreadsheet.open './posts_data.xls'
+
+sheet1 = book.worksheet 0
+
+sheet1.each do |row|
+  post_info = {}
+    post_info[:date] = row[4]
+    post_info[:user_id] = row[3]
+    post_info[:body] = row[7]
+    post_info[:variety] = row[9]
+
+    Post.create(post_info)
+end
